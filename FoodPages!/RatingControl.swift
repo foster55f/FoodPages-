2ec35483ce59786ef156ed6f383a30bae0c78ac0
“Foster <fosterataylor@yahoo.com>
2020-04-16 16:13:45 -0600

    Add button action for a user to enter a start rating

--- FoodPages!/RatingControl.swift
+++ FoodPages!/RatingControl.swift
@@ -12,6 +12,12 @@ class RatingControl: UIStackView {
     setupButtons()
     }
     
+    //MARK: Button Action
+    
+   @objc func ratingButtonTapped(button: UIButton) {
+        print("Button pressed 👍")
+    }
+    
     //MARK: Private Methods
     
     private func setupButtons() {
@@ -23,6 +29,9 @@ class RatingControl: UIStackView {
         button.translatesAutoresizingMaskIntoConstraints = false
         button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
         button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+        
+        // Setup the button action
+        button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
         
         // Add the button to the stack
         addArrangedSubview(button)
