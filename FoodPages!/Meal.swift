68415fb7f7bdf8759ce86ebbbe39e3bcc56b68c5
“Foster <fosterataylor@yahoo.com>
2020-04-17 12:19:07 -0600

    Add Meal.swift class

---
+++ FoodPages!/Meal.swift
@@ -0,0 +1,37 @@
+//
+//  Meal.swift
+//  FoodPages!
+//
+//  Created by Foster Taylor on 4/17/20.
+//  Copyright © 2020 Foster Taylor. All rights reserved.
+//
+
+import UIKit
+
+class Meal {
+    
+    //MARK: Properties
+    
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    
+    //MARK: Initialization
+     
+    init?(name: String, photo: UIImage?, rating: Int) {
+        
+        // Initialization should fail if there is no name or if the rating is negative.
+        if name.isEmpty || rating < 0  {
+            return nil
+        }
+        
+        // Initialize stored properties.
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+        
+    }
+
+}
+
