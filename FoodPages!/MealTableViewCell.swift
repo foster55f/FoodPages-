524507903fd93ad3197d6ee1300f47e409bab196
“Foster <fosterataylor@yahoo.com>
2020-04-17 14:02:13 -0600

    Create table view

---
+++ FoodPages!/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  FoodPages!
+//
+//  Created by Foster Taylor on 4/17/20.
+//  Copyright © 2020 Foster Taylor. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
