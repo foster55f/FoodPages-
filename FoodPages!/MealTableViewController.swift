c82a8a998ec7ab63d97b14ad4a1e6a4963633d6e
“Foster <fosterataylor@yahoo.com>
2020-05-04 10:14:27 -0600

    Edit meals

--- FoodPages!/MealTableViewController.swift
+++ FoodPages!/MealTableViewController.swift
@@ -135,15 +135,15 @@ class MealTableViewController: UITableViewController {
         let photo2 = UIImage(named: "meal2")
         let photo3 = UIImage(named: "meal3")
         
-        guard let meal1 = Meal(name: "Caprese Salad", photo: photo1, rating: 4) else {
+        guard let meal1 = Meal(name: "Caesar Salad", photo: photo1, rating: 4) else {
             fatalError("Unable to instantiate meal1")
         }
          
-        guard let meal2 = Meal(name: "Chicken and Potatoes", photo: photo2, rating: 5) else {
+        guard let meal2 = Meal(name: "Chicken and Apples", photo: photo2, rating: 5) else {
             fatalError("Unable to instantiate meal2")
         }
          
-        guard let meal3 = Meal(name: "Pasta with Meatballs", photo: photo3, rating: 3) else {
+        guard let meal3 = Meal(name: "Pasta with Meat", photo: photo3, rating: 3) else {
             fatalError("Unable to instantiate meal2")
         }
         meals += [meal1, meal2, meal3]
